4ac9c969901f550bdef35bc82d5c23995f2fb5cc
khoavu <khoavu@dogoo.vn>
2025-03-18 13:55:35 +0700

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl bg-red-700 font-bold underline">
+      React 19 with Tailwind CSS v4
+    </h1>
+  );
+}
+
+export default App;
